67929586661481813fe3e0abb6b20a1b0172c16e
FOR-PARADOX <49624929+FOR-PARADOX@users.noreply.github.com>
2019-06-23 18:13:44 -0700

    stable opensourced

---
+++ OpenBullet/Pages/Main/About.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace OpenBullet
+{
+    /// <summary>
+    /// Logica di interazione per About.xaml
+    /// </summary>
+    public partial class About : Page
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void orepoButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/FOR-PARADOX/REBO-");
+        }
+        private void repoButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/FOR-PARADOX/REBO-");
+        }
+
+        private void docuButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Process.Start("https://crackingparadox.com/threads/simple-blackbullet-config-tutorial.4211/");
+        }
+
+        private void configButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Process.Start("https://crackingparadox.com/forums/configs.19/");
+        }
+    }
+}
